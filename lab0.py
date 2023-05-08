f62743c3f4a48b546629d44402e0452a9dc4249a
Kudzai Zhuwaki <125462164+KudzaiNovae@users.noreply.github.com>
2023-05-08 15:16:26 +0200

    Lab 0: Workflow and Python Basics
    Lab 0: Workflow and Python Basics

---
+++ lab0.py
@@ -0,0 +1,11 @@
+#Replace with your email. Surround it by double quotes
+email = "N02126464Q@students.nust.ac.zw" 
+
+def twenty_twenty_three():
+    """Come up with the most creative expression that evaluates to 2023,
+    using only numbers and the +, *, and - operators. Use all three of 
+    them only to get full credit. Do NOT use division.
+    >>> twenty_twenty_three()
+    2023
+    """
+    return (2 * 1000) + (3 * 100) - (5 * 6) - (13 * 19)
